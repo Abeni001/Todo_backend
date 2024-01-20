f5330310d3c3059b6a265eca449635742aafebf3
Abenezer Getnet <getnetabenezar@gmail.com>
2024-01-20 12:20:51 +0300

    enviroment updated for deploymnet

--- key.js
+++ key.js
@@ -1,3 +1,3 @@
 export const PORT = process.env.PORT || 3000
 export const MONGO_URI= process.env.MONGOURI || "mongodb://localhost:27017/TodoApp"
-export const SECRET_STR = "abenilovesconding@2016"
+export const SECRET_STR =process.env.SECRET_STR || "abenezergetnet-lovestocode-verymuch-butitstruggles-IN2016EC"
