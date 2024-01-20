e27e4807124dc125bc3f06f956a7c40f52a4c0f8
Abenezer Getnet <getnetabenezar@gmail.com>
2024-01-20 11:42:10 +0300

    middlewarre added to it

--- routes/todoRoute.js
+++ routes/todoRoute.js
@@ -1,14 +1,15 @@
 import express from "express";
 import { getAllTodos,store,show,destroy,update } from "../controllers/todoController.js";
+import { authMiddleware } from "../middlewares/auth.js";
 const router = express.Router()
 
 router.route('/')
                 .get(getAllTodos)
-                .post(store)
+                .post(authMiddleware,store)
 
 router.route('/:id')
                     .get(show)
-                    .put(update)
-                    .delete(destroy)
+                    .put(authMiddleware,update)
+                    .delete(authMiddleware,destroy)
 
 export default router
